d85906d3cc2d6f7a6b93b6ae0ac0ee86ef2d58d3
MatthijsSchokker <39410132+MatthijsSchokker@users.noreply.github.com>
2018-05-18 16:08:38 +0200

    Add files via upload

---
+++ DatabaseConnector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Schokkers_Biology_Quiz
+{
+    class DatabaseConnector
+    {
+        private SqlConnection myConn;
+        private SqlDataAdapter sqlDataAdapter;
+        private string connectionString;
+
+        public string conStr
+        {
+            set { connectionString = value; }
+        }
+
+        public System.Data.DataSet AnimalQuizData
+        {
+            get { return AnimalQuestions(); }
+        }
+
+        private System.Data.DataSet AnimalQuestions()
+        {
+            using (myConn = new SqlConnection(connectionString))
+            {
+                myConn.Open();
+                sqlDataAdapter = new SqlDataAdapter("Select * From animalTable", myConn);
+
+                System.Data.DataSet dataS = new System.Data.DataSet();
+
+                sqlDataAdapter.Fill(dataS, "animalTable");
+
+                return dataS;
+            }
+        }
+
+      public void UpdateDatabase(System.Data.DataSet dataS)
+        {
+            using (myConn = new SqlConnection(connectionString))
+            {
+                myConn.Open();
+                sqlDataAdapter = new System.Data.SqlClient.SqlDataAdapter("Select * From animalTable", myConn);
+
+                SqlCommandBuilder objCommandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+                sqlDataAdapter.Update(dataS, "animalTable");
+            }
+        }
+
+    }
+}
---
+++ LoadQuestion.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace Schokkers_Biology_Quiz
+{
+    class LoadQuestion
+    {        
+        private List<int> selectedTopics;
+
+        string conString;
+        DataSet dataS;
+
+        Random rnd = new Random();
+        int maxR;
+                
+        private List<string> answers = new List<string>();
+        private List<int> currentQ = new List<int>();
+        private object picture = new Object();
+        private int currentA;
+        
+        public List<string> NextQuestion()
+        {
+            currentQ.Clear();
+            answers.Clear();
+
+            if (dataS == null)
+            {
+                //========================================
+                // Connectie met vragendatabase
+                //========================================
+
+                DatabaseConnector connector = new DatabaseConnector();
+
+                conString = Properties.Settings.Default.QuizDataString;
+
+                connector.conStr = conString;
+
+                dataS = connector.AnimalQuizData;
+
+                //========================================
+                // opstellen parameters
+                //========================================
+
+                maxR = dataS.Tables[0].Rows.Count;
+            }
+
+            //========================================
+            // bepalen antwoordmogelijkheden
+            //========================================
+
+            while (currentQ.Count < 4)
+            {
+                int rndA = rnd.Next(0, maxR - 1);
+                if (!currentQ.Contains(rndA))
+                {
+                    currentQ.Add(rndA);
+                }
+            }
+            
+            foreach (int i in currentQ)
+            {
+                answers.Add(dataS.Tables[0].Rows[i][1].ToString());
+            }
+
+            //========================================
+            // kies het goede antwoord
+            //========================================
+
+            int cA = rnd.Next(0, 3);
+
+            currentA = currentQ[cA];
+
+            return answers;
+        }
+                    
+        public string CorrectAnswer()
+        {
+            int cA = currentQ.IndexOf(currentA);
+            return answers[cA];
+        }
+
+        public object LoadImage()
+        {
+            picture = (Byte[])(dataS.Tables[0].Rows[currentA][3]);
+            return picture;
+        }
+    }
+}
